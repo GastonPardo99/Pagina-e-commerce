98b2e91da51130d45fe11c9f2dba569934833da6
gastonpardo7599 <gastpardo99@gmail.com>
2020-08-05 13:24:50 -0300

    redireccionar
    login

--- js/init.js
+++ js/init.js
@@ -47,4 +47,5 @@ var redireccion_autenticacion = function(){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  redireccion_autenticacion()
 });

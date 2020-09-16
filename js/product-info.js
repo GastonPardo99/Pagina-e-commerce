bd2f46385e254205aee818d6f1c3d51080f7c3ba
gastonpardo7599 <gastpardo99@gmail.com>
2020-09-16 14:24:28 -0300

    carrousel

--- js/product-info.js
+++ js/product-info.js
@@ -33,24 +33,57 @@ function showProductInfo(array){
     
     for(let i = 0; i < array.images.length; i++){
         let productImage = array.images[i];
-    
 
+    if (i==0) {
         htmlContentToAppendImages += `
-        
-        <div>
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + productImage + `"  class="imagenesgaleria" >
+                <div class="carousel-item active" >
+                    <img src="` + productImage + `"  class="imagenesgaleria d-block w-100" >
                 </div>
-            </div>
-        </div>
-  
         ` 
+    
+    } else {
+        htmlContentToAppendImages += `
+                <div class="carousel-item" >
+                    <img src="` + productImage + `"class="imagenesgaleria d-block w-100" >
+                </div>
+        ` 
+    }
+    
+    }
+     let htmlContentToAppendCarrousel = ""
 
-        document.getElementById("car-list-container").innerHTML = htmlContentToAppend + htmlContentToAppendImages;
+        htmlContentToAppendCarrousel += `
+        <div class="achicar">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            </ol>
+            <div class="carousel-inner">
+             ` + htmlContentToAppendImages + `
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        </div>
+
+        `
+        
+
+        document.getElementById("car-list-container").innerHTML = htmlContentToAppend + htmlContentToAppendCarrousel;
+
 
 }
-}
+
 
 //Funcion de mostrar los comentarios sobre el producto
 function showComentary(array){

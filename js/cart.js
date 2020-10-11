71fa5fc3afbe10cbab68a699f49813fb7508d99e
gastonpardo7599 <gastpardo99@gmail.com>
2020-10-10 21:05:22 -0300

    Funcionalidad del carrito

--- js/cart.js
+++ js/cart.js
@@ -1,111 +1,107 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-var carritoArray = [];
-var alerta = [];
+var articulosArray = [];
 
 
-function showCarrito(array){
+function showArticulosCarrito(array){
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++){
+        let article = array[i];
 
-    let htmlContentToAppendCarrito = "";
+        if(article.currency === "UYU"){
 
-        htmlContentToAppendCarrito += `
-        <div>
-            <h3>`+ array.articles[0].name +`</h3>
-            <img src="`+ array.articles[0].src +`">
-        </div>
-        <div>
-            <form>
-
-                <fieldset>
-                <label for="precio_producto"> Precio unitario (UYU) = </label>
-                <input type="number" id="precio_producto" value="` + array.articles[0].unitCost +`" disabled>
-                </fieldset>
-                <label for="cantidad_producto"> Cantidad : </label>
-                <input type="number" id="cantidad_producto" value="` + array.articles[0].count + `" onclick="subtotal()"required>
-
-                <p id="subtotal" class="costos"> Subtotal : ` + array.articles[0].unitCost  *  array.articles[0].count +`<p>
-
-                <fieldset class="datos_envio">
-                    <label name="Tipo de Envo" value=""><b>Tipo de envío (Seleccionar)</b></label>
-                    <br>
-                    <input id="envio" type="radio" name="Tipo de Envio"   onclick="total(1.05)"> Standard (12 a 15 días)- 5% 
-                    <br>
-                    <input id="envio2" type="radio" name="Tipo de Envio"  onclick="total(1.07)"> Express (5 a 8 días)- 7% 
-                    <br>
-                    <input id="envio3" type="radio" name="Tipo de Envio"  onclick="total(1.15)"> Premium(12 a 15 días)- 15% 
-                </fieldset>
-
-                <p id="total" class="costos">Total (UYU) = <p>
-
-            </form>
+             article.unitCost = 0.025 * article.unitCost 
+        }
         
-        <div>
-            <form>
-                <fieldset class="datos_envio">
-                <label for="Direccion"><b> Direccion : </b></label>
-                <input type="text" id="Direccion">
-                </fieldset>
+        htmlContentToAppend += `<div class="container mb-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="tabla"> </th>
+                                <th scope="col" class="tabla">Producto</th>
+                                <th scope="col" class="tabla" type="number">Cantidad</th>
+                                <th scope="col" class="tabla">Precio (USD)</th>
+                                <th> </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="tabla"><img src="` + article.src + `" / class="imagen_carrito"> </td>
+                                <td class="tabla">` + article.name + `</td>
+                                <td class="tabla"><label id="label-cantidad` + i + `"></label> <input id="cantidad` + i + `" type="number" class="form-control" value="` + article.count + `"></td>
+                                <td class="tabla">` + article.unitCost + `</td>
+                                <td class="tabla"></td>
+                                </tr>
+                                <tr>
+                                <td colspan="4"></td>
+                                <td colspan="2">SUBTOTAL (USD) : <input type="number" id="subtotal` + i + `" disabled></td>
+                            </tr>
+                                `
+        
+    }
 
-                <fieldset>
-                <label for="Pais"><b> País : </b></label>
-                <input type="text" id="Pais">
-                </fieldset>
-
-                <fieldset class="datos_envio">
-                    <label for="forma_pago" name="forma de pago"><b>Forma de pago</b></label>
-                    <br>
-                    <input type="radio"  name="forma de pago" value="1"> Tarjeta de crédito
-                    <br>
-                    <input type="radio"  name="forma de pago" value="2"> Transferencia bancaria
-                </fieldset>
-
-                <input type="button"  value="Comprar" class="costos" onclick="compraExitosa()">
-
-            <form>
-        </div>
-
-            
-        </div>
-
-        `
-
-        document.getElementById("carrito_info").innerHTML = htmlContentToAppendCarrito;
+    document.getElementById("carrito").innerHTML = htmlContentToAppend;
     
+
 }
 
-function subtotal() {
-    let cantidad = document.getElementById("cantidad_producto").value
-    let precio = document.getElementById("precio_producto").value
-    let subtotal = cantidad * precio
-    document.getElementById("subtotal").innerHTML = "Subtotal : " + subtotal
-}
 
-function total(envio){
-    let cantidad = document.getElementById("cantidad_producto").value
-    let precio = document.getElementById("precio_producto").value
-    let total = cantidad * precio * envio 
-    document.getElementById("total").innerHTML = "Total (UYU) = " + total
-}
 
 function compraExitosa(){
     mensaje = alerta.msg
     alert(mensaje)
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-    showSpinner();
-    getJSONData(CART_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            carritoArray = resultObj.data
-            showCarrito(carritoArray);
+document.addEventListener("DOMContentLoaded", function (e) {
+    
+    getJSONData(CART_2).then(function(resultObj){
+        
+        if (resultObj.status === "ok");
+        {
+            articulosArray = resultObj.data.articles;
+            showArticulosCarrito(articulosArray);
+           
         }
+
+        function sumatoriaTotal(){
+            var total = 0;
+            for (let i=0; i<articulosArray.length;i++){
+                // subtotales += document.getElementById("subtotal"+ i).value;
+                total += parseInt(document.getElementById("subtotal"+ i).value); 
+                console.log(subtotales);
+            }
+            document.getElementById("subtotales").innerHTML= total;
+        }
+        
+        function costo() {
+            for (let i=0; i<articulosArray.length;i++){
+                var cantidad = document.getElementById("cantidad" + i).value;
+                var article = articulosArray[i];
+            var precio   = article.unitCost;
+            var cuenta = parseInt(cantidad) * precio;
+            document.getElementById("subtotal"+i).value = cuenta;
+                
+            }         
+            
+            sumatoriaTotal();
+            
+        
+        }
+
+        for (let i=0; i<articulosArray.length;i++){
+            document.getElementById("cantidad"+i).addEventListener("change",costo);
+
+        }
+
+        
+       
     });
     getJSONData(CART_BUY_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             alerta = resultObj.data
         }
     });
-    hideSpinner();
-});
+
+})
 

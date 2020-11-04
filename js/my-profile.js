0c3f81be1084bb4e407e615746dfbb21dfed9b54
gastonpardo7599 <gastpardo99@gmail.com>
2020-11-04 19:42:42 -0300

    Validación email desde js

--- js/my-profile.js
+++ js/my-profile.js
@@ -9,37 +9,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
     document.getElementById("correo").value = User.correo;
     document.getElementById("telefono").value = User.telefono ;
     
-        document.getElementById("editarDatos").addEventListener("click", () =>{
-        document.getElementById("nombre").removeAttribute("disabled");
-        document.getElementById("edad").removeAttribute("disabled");
-        document.getElementById("correo").removeAttribute("disabled");
-        document.getElementById("telefono").removeAttribute("disabled");
+    document.getElementById("editarDatos").addEventListener("click", () =>{
+    document.getElementById("nombre").removeAttribute("disabled");
+    document.getElementById("edad").removeAttribute("disabled");
+    document.getElementById("correo").removeAttribute("disabled");
+    document.getElementById("telefono").removeAttribute("disabled");
     })
 });
-
-function validar(event){
-
-let nombreInput = document.getElementById("nombre").value;
-let nombreExpress = /^[a-zA-ZÀ-ÿ\ñ\Ñ]+(\s*[a-zA-ZÀ-ÿ\ñ\Ñ]*)*[a-zA-ZÀ-ÿ\ñ\Ñ]+$/
-let celularInput = document.getElementById("telefono").value;
-let celularExpress = /^\d{9,12}$/;
-
-
-if (!nombreExpress.test(nombreInput)){
-    alert("Acepta solo caracteres");
-    event.preventDefault();
-} 
-if ((document.getElementById("edad").value) < 18) {
-    alert("Debes ser mayor de 18");
-    event.preventDefault();
-} 
-if (!celularExpress.test(celularInput)){
-    alert("El número de celular debe ser entre 9 y 12 dígitos");
-    event.preventDefault();
-} 
-
-guardarDatos()
-}
 
 function guardarDatos(){
     
@@ -58,6 +34,32 @@ function guardarDatos(){
     document.getElementById("correo").setAttribute("disabled", "");
     document.getElementById("telefono").setAttribute("disabled", "");
 
+}
 
+function validar(event){
+
+    let nombreInput = document.getElementById("nombre").value;
+    let nombreExpress = /^[a-zA-ZÀ-ÿ\ñ\Ñ]+(\s*[a-zA-ZÀ-ÿ\ñ\Ñ]*)*[a-zA-ZÀ-ÿ\ñ\Ñ]+$/
+    let celularInput = document.getElementById("telefono").value;
+    let celularExpress = /^\d{9,12}$/;
+    let emailInput = document.getElementById("correo").value;
+    let emailExpress = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;   
+
+    if (!nombreExpress.test(nombreInput)){
+        alert("Acepta solo caracteres");
+        event.preventDefault();
+    } else if ((document.getElementById("edad").value) < 18) {
+        alert("Debes ser mayor de 18");
+        event.preventDefault();
+    } else if (!emailExpress.test(emailInput)){
+        alert ("Dirección de correo inválida");
+        event.preventDefault();
+    } else if (!celularExpress.test(celularInput)){
+        alert("El número de celular debe ser entre 9 y 12 dígitos");
+        event.preventDefault();
+    } else{
+        guardarDatos()
+    }
 
 }
+
